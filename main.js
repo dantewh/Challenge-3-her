ced79c9bb6c2a552366f8ff7dc40cebd2ebcd760
dantewh <61277342+dantewh@users.noreply.github.com>
2020-06-29 14:04:10 +0200

    https testen

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onload = function myFunction() {
 };
 
 
-fetch('http://weerlive.nl/api/json-data-10min.php?key=4c9967ff90&locatie=Amsterdam&callback=?', {
+fetch('https://weerlive.nl/api/json-data-10min.php?key=4c9967ff90&locatie=Amsterdam&callback=?', {
     
 })
     .then(res => res.json())
@@ -12,7 +12,7 @@ fetch('http://weerlive.nl/api/json-data-10min.php?key=4c9967ff90&locatie=Amsterd
 
 
 
-fetch('http://weerlive.nl/api/json-data-10min.php?key=4c9967ff90&locatie=Amsterdam&callback=?', {
+fetch('https://weerlive.nl/api/json-data-10min.php?key=4c9967ff90&locatie=Amsterdam&callback=?', {
     
 })
     .then(res => res.json())
