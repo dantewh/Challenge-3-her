4610744b66b675e0ff3ea85511cc4bd01dcb7d02
dantewh <61277342+dantewh@users.noreply.github.com>
2020-06-29 13:04:43 +0200

    Api key aangepast
    { } om de key weggehaald na tips docent

---
+++ README.md
@@ -0,0 +1,10 @@
+# Challenge 3
+ 
+## welkom op mijn repository 
+
+Dit is een repository over het gebruiken van api's in javascript. Hier is een weer api gebruikt die de windkracht en richting verteld, ook is er een kaart te zien met de ziekenhuizen in de buurt van amsterdam.
+
+[Dit is trouwens mijn website](http://www.dhondelink.nl)
+
+**Linkjes lukken nog nietecht** **oh toch wel**,
+**Bolt** en *italic ook*
---
+++ main.js
@@ -0,0 +1,59 @@
+window.onload = function myFunction() {
+    document.getElementsByClassName('icons')[0].style.visibility = 'hidden';
+};
+
+
+fetch('http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam&callback=?', {
+    
+})
+    .then(res => res.json())
+
+    .then(data => document.getElementById('windkmh').innerHTML = (data['liveweer'][0]['windkmh']))
+
+
+
+fetch('http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam&callback=?', {
+    
+})
+    .then(res => res.json())
+
+    .then(data => document.getElementById('windr').innerHTML = (data['liveweer'][0]['windr']))
+
+
+var platform = new H.service.Platform({
+  'apikey': 'vRBJhaT_Y0LX-KEs_3njwkWU75tnlpRpEROVBGbmGM8'
+});
+
+var defaultLayers = platform.createDefaultLayers();
+
+
+var map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,
+    {
+      zoom: 11,
+      center: { lat: 52.328362, lng: 4.904633 }
+        
+    });
+
+function addMarkersToMap(map) {
+    var bijvenIjMarker = new H.map.Marker({lat:52.404563, lng:4.922179});
+    map.addObject(bijvenIjMarker);
+
+    var olvgMarker = new H.map.Marker({lat:52.374827, lng: 4.837848});
+    map.addObject(olvgMarker);
+
+    var pietHeinMarker = new H.map.Marker({lat:52.376819, lng:4.927620});
+    map.addObject(pietHeinMarker);
+
+    var amcMarker = new H.map.Marker({lat:52.297377, lng: 4.972733});
+    map.addObject(amcMarker);
+
+    
+}
+
+window.addEventListener('resize', () => map.getViewPort().resize());
+
+window.onload = function () {
+  addMarkersToMap(map);
+}      
